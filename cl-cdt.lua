d7406b43b4348b1f24d9423e113f4b8910164adf
Kernel <kernelnotfound@duck.com>
2026-07-30 05:37:45 -0300

    Update cl-cdt.lua

--- cl-cdt.lua
+++ cl-cdt.lua
@@ -1,1 +1,18 @@
-
+return {
+    {
+        version = "v1.0 Release",
+        date = "30/07/2026",
+        changes = {
+            { type = "Corrigido", text = "Sistema de alinhamento fiel ao do game." },
+            { type = "Adicionado", text = "Trava de velocidade." },
+            { type = "Adicionado", text = "Editor de mesh base by @maxx_544 (será completamente reformulado e terá melhorias próprias em breve)." },
+        },
+    },
+    {
+        version = "Beta",
+        date = "28/07/2026",
+        changes = {
+            { type = "Adicionado", text = "Funções de movimento, gravidade, shaders e alinhamento base by @antipathico03 (discord)." },
+        },
+    },
+}
