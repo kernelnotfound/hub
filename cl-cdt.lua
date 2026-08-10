0a6e73fcc46b11fd173099825c7daed8dac0854a
Kernel <kernelnotfound@duck.com>
2026-08-10 01:11:30 -0300

    Update cl-cdt.lua

--- cl-cdt.lua
+++ cl-cdt.lua
@@ -1,18 +1,20 @@
 return {
     {
-        version = "v1.0 Release",
-        date = "30/07/2026",
+        version = "v1.0",
+        date = "09/08/2026",
         changes = {
-            { type = "Corrigido", text = "Sistema de alinhamento fiel ao do game." },
-            { type = "Adicionado", text = "Trava de velocidade." },
-            { type = "Adicionado", text = "Editor de mesh base by @maxx_544 (será completamente reformulado e terá melhorias próprias em breve)." },
+            { type = "Adicionado", text = "Editor de Sons do Carro (presets de motor, ID customizado, volume e efeitos)." },
+            { type = "Adicionado", text = "Sistema de Compartilhamento de Configs de Som (Exportar/Importar via clipboard)." },
+            { type = "Adicionado", text = "Editor de meshes customizado." },
+            { type = "Adicionado", text = "Trava de velocidade, suspensão, gravidade, movimento e shaders." },
+            { type = "Alterado", text = "Portado para a nova Black UI Library com gerenciador de janelas." },
         },
     },
     {
         version = "Beta",
         date = "28/07/2026",
         changes = {
-            { type = "Adicionado", text = "Funções de movimento, gravidade, shaders e alinhamento base by @antipathico03 (discord)." },
+            { type = "Adicionado", text = "Funções de movimento, gravidade, shaders e alinhamento." },
         },
     },
 }
